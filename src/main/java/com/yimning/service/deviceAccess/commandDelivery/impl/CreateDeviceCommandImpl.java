26d636aca92cba7e6e3ffd59ca6fc8736afd836a
Yimning <1148967988@qq.com>
2021-04-05 19:34:27 +0800

    修改完善代码—查询设备的历史数据

---
+++ src/main/java/com/yimning/service/deviceAccess/commandDelivery/impl/CreateDeviceCommandImpl.java
@@ -0,0 +1,103 @@
+package com.yimning.service.deviceAccess.commandDelivery.impl;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yimning.utils.Constant;
+import com.yimning.utils.HttpsUtil;
+import com.yimning.utils.JsonUtil;
+import com.yimning.utils.StreamClosedHttpResponse;
+import org.apache.http.HttpResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Creating Device Commands :
+ * 
+ * The device profile file defines commands that the IoT platform can deliver to a device. 
+ * When an NA needs to configure or modify the service attributes of a device, the NA can 
+ * call this API to deliver commands to the device.
+ */
+public class CreateDeviceCommandImpl {
+
+    public static void main(String[] args) throws Exception {
+
+        // Two-Way Authentication
+        HttpsUtil httpsUtil = new HttpsUtil();
+        httpsUtil.initSSLConfigForTwoWay();
+
+        // Authentication.get token
+        String accessToken = login(httpsUtil);
+
+        //Please make sure that the following parameter values have been modified in the Constant file.
+        String urlCreateDeviceCommand = Constant.CREATE_DEVICE_CMD;
+        String appId = Constant.APPID;
+        String callbackUrl = Constant.REPORT_CMD_EXEC_RESULT_CALLBACK_URL;
+
+        //please replace the deviceId, when you call this interface.
+        String deviceId = "de71ad4a-211e-4e53-b48c-80cb2d5c88c8";
+        //please replace the following parameter values as required, when you call this interface.
+        Integer expireTime = 0;
+        Integer maxRetransmit = 3;
+
+        //please replace the following parameter values, when you call this interface.
+        //And those parameter values must be consistent with the content of profile that have been preset to IoT platform.
+        //The following parameter values of this demo are use the watermeter profile that already initialized to IoT platform.
+        String serviceId = "WaterMeter";
+        String method = "CHANGE_STATUS";
+        ObjectNode paras = JsonUtil.convertObject2ObjectNode("{\"value\":\"1\"}");
+      
+        Map<String, Object> paramCommand = new HashMap<>();
+        paramCommand.put("serviceId", serviceId);
+        paramCommand.put("method", method);
+        paramCommand.put("paras", paras);
+        
+        Map<String, Object> paramCreateDeviceCommand = new HashMap<>();
+        paramCreateDeviceCommand.put("deviceId", deviceId);
+        paramCreateDeviceCommand.put("command", paramCommand);
+        paramCreateDeviceCommand.put("callbackUrl", callbackUrl);
+        paramCreateDeviceCommand.put("expireTime", expireTime);
+        paramCreateDeviceCommand.put("maxRetransmit", maxRetransmit);
+        
+        String jsonRequest = JsonUtil.jsonObj2Sting(paramCreateDeviceCommand);
+                
+        Map<String, String> header = new HashMap<>();
+        header.put(Constant.HEADER_APP_KEY, appId);
+        header.put(Constant.HEADER_APP_AUTH, "Bearer" + " " + accessToken);
+        
+        HttpResponse responseCreateDeviceCommand = httpsUtil.doPostJson(urlCreateDeviceCommand, header, jsonRequest);
+
+        String responseBody = httpsUtil.getHttpResponseBody(responseCreateDeviceCommand);
+
+        System.out.println("CreateDeviceCommand, response content:");
+        System.out.println(responseCreateDeviceCommand.getStatusLine());
+        System.out.println(responseBody);
+        System.out.println();
+    }
+
+    /**
+     * Authentication.get token
+     */
+    @SuppressWarnings("unchecked")
+    public static String login(HttpsUtil httpsUtil) throws Exception {
+
+        String appId = Constant.APPID;
+        String secret = Constant.SECRET;
+        String urlLogin = Constant.APP_AUTH;
+
+        Map<String, String> paramLogin = new HashMap<>();
+        paramLogin.put("appId", appId);
+        paramLogin.put("secret", secret);
+
+        StreamClosedHttpResponse responseLogin = httpsUtil.doPostFormUrlEncodedGetStatusLine(urlLogin, paramLogin);
+
+        System.out.println("app auth success,return accessToken:");
+        System.out.println(responseLogin.getStatusLine());
+        System.out.println(responseLogin.getContent());
+        System.out.println();
+
+        Map<String, String> data = new HashMap<>();
+        data = JsonUtil.jsonString2SimpleObj(responseLogin.getContent(), data.getClass());
+        return data.get("accessToken");
+    }
+
+}
